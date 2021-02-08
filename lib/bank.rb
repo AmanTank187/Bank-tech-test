760a0a2c7a14d0ca7f71e6e7430338ecd8ea7152
Aman Tank <amantank187@gmail.com>
2021-02-08 17:14:24 +0000

    Allows user to see their bankstatement

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,18 +1,38 @@
+
 class Bank 
 
-    attr_reader :balance
+    attr_reader :balance, :bank_statement_array
 
     def initialize 
         @balance = 0
+        @bank_statement_array = []
     end 
 
     def deposit(date,amount)  
         @balance += amount 
-        
+        saving_bank_action_data(date,amount)
     end 
 
     def withdraw(date,amount)  
-        @balance -= amount 
+        @balance -= amount
+        saving_bank_action_data(date,-1 * amount)
+    end 
+
+    def show_bank_statement
+        puts "date || credit || debit || balance"
+        @bank_statement_array.reverse.each do |i|
+            if i[1].negative?
+                puts "#{i[0]} || || #{-1 * i[1]}  || #{i[2]}"
+            else 
+                puts "#{i[0]} || #{i[1]} ||  || #{i[2]}"
+            end 
+        end 
+    end 
+
+private 
+
+    def saving_bank_action_data(date,amount)
+        @bank_statement_array << ([date,amount,self.balance])
     end 
 
 end 
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,8 +1,11 @@
 require 'bank'
 
 describe Bank do
-    #Using this day variable to pass in as a date argument for the deposit and withdraw functions 
+    #Using this day variables to pass in as a date argument for the deposit and withdraw functions 
     day = '14/01/2012' 
+    day_two = '18/01/2012'
+    day_three = '24/01/2012'
+
     it 'Is an instance of the bank class' do
         expect(subject).to be_instance_of (Bank)
     end 
@@ -15,17 +18,35 @@ describe Bank do
 
     describe 'deposit' do
         it 'Depositing 500 will make the balance change to 500' do
-            subject.deposit(day,500)
-            expect(subject.balance).to eq 500
+            subject.deposit(day,500.0)
+            expect(subject.balance).to eq 500.0
+            expect(subject.bank_statement_array).to eq ([['14/01/2012',500.0,500.0]])
         end 
     end 
 
     describe 'withdraw' do
         it 'Withdrawing 500 from a total of 1500 will change the balance to 1000' do
-            subject.deposit(day,1500)
-            subject.withdraw(day,500)
-            expect(subject.balance).to eq 1000
+            subject.deposit(day,1500.0)
+            subject.withdraw(day,500.0)
+            expect(subject.balance).to eq 1000.0
+            expect(subject.bank_statement_array).to eq ([['14/01/2012',1500.0,1500.0],['14/01/2012',-500.0,1000.0]])
         end 
     end 
+
+    describe 'show_bank_statement' do
+
+        bank_statement_string = "date || credit || debit || balance\n24/01/2012 || || 500.0  || 2000.0\n18/01/2012 || 1000.0 ||  || 2500.0\n14/01/2012 || 1500.0 ||  || 1500.0\n"
+
+        it 'Should print out the bank statement' do
+            subject.deposit(day,1500.0)
+            subject.deposit(day_two,1000.0)
+            subject.withdraw(day_three,500.0)
+            expect{ subject.show_bank_statement }.to output(bank_statement_string).to_stdout
+        end 
+
+    
+    end 
+
+  
 
 end 
