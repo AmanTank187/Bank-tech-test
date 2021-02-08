262c9f1a03521a3e7e9c9d19b7b83067704f595c
Aman Tank <amantank187@gmail.com>
2021-02-08 11:37:25 +0000

    Added a balance instance variable and a function that gets the balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,10 @@
 class Bank 
 
+    def initialize 
+        @balance = 0
+    end 
+
+    def get_balance
+        @balance
+    end 
 end 
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,4 +4,10 @@ describe Bank do
     it 'Is an instance of the bank class' do
         expect(subject).to be_instance_of (Bank)
     end 
+
+    it 'It has a balance of 0 when the class is made' do
+        newBank = Bank.new 
+        expect(newBank.get_balance).to eq 0
+    end 
+
 end 
